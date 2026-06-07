37417fe476a29dfac16a5b63c982eae3c8835879
thraneh <thraneh@gmail.com>
2026-06-07 05:29:41 +0000

    chore

---
+++ src/roq/fix_bridge/settings.hpp
@@ -0,0 +1,13 @@
+/* Copyright (c) 2017-2026, Hans Erik Thrane */
+
+#pragma once
+
+#include "roq/fix_bridge/flags/settings.hpp"
+
+namespace roq {
+namespace fix_bridge {
+
+using Settings = flags::Settings;
+
+}  // namespace fix_bridge
+}  // namespace roq
